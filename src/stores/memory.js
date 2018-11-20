09bf71b52da2f7c45e59e044106efe4239bd9361
Jesse Kriss <jesse@jklabs.net>
2018-11-20 13:24:51 -0800

    Adding faster hash checking to the memory store

--- src/stores/memory.js
+++ src/stores/memory.js
@@ -3,13 +3,17 @@ const filter = require('../util/filter')
 class MemoryStore {
   constructor() {
     this.messages = []
+    this.hashes = {}
   }
   async exists(message) {
-    return !!this.messages.find(m => m.meta.hash === message.meta.hash)
+    return this.hashes[message.meta.hash]
   }
   async write(message) {
     const exists = await this.exists(message)
-    if (!exists) this.messages.push(message)
+    if (!exists) {
+      this.messages.push(message)
+      this.hashes[message.meta.hash] = true
+    }
   }
   async get(hash) {
     const m = this.messages.find(m => m.meta.hash === hash)
@@ -21,12 +25,14 @@ class MemoryStore {
   }
   async del(hash) {
     const idx = this.messages.findIndex(m => m.meta.hash === hash)
+    delete this.hashes[hash]
     if (idx >= 0) {
       this.messages.splice(idx, 1)
     }
   }
   async clear() {
     this.messages = []
+    this.hashes = {}
   }
   *filteredGenerator(q) {
     const f = filter(q)
