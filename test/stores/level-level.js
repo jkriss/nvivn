aa479e658cae486d91ced155d8a46c7e8e0248a7
Jesse Kriss <jesse@jklabs.net>
2018-11-23 23:18:56 -0800

    Making sure the test leveldb dir is present

--- test/stores/level-level.js
+++ test/stores/level-level.js
@@ -5,8 +5,9 @@ const fs = require('fs-extra')
 const path = require('path')
 
 const testDataDB = path.join(__dirname, '..', 'tmp', 'messages.db')
+fs.ensureDir(testDataDB).then(() => {
+  const db = level(testDataDB)
 
-const db = level(testDataDB)
-
-const checkFrequency = 10
-genericStoreTester(LevelStore, { db, checkFrequency })
+  const checkFrequency = 10
+  genericStoreTester(LevelStore, { db, checkFrequency })
+})
